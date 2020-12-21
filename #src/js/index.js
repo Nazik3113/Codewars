d174fcd2d690a49df5562e3779ac0855b4e1afa4
Nazariy <nazarroot3113@gmail.com>
2020-12-21 19:01:36 +0200

    findEvenIndex

--- #src/js/index.js
+++ #src/js/index.js
@@ -3,5 +3,39 @@ import webp from './modules/webp';
 window.addEventListener('DOMContentLoaded', () => {
   webp();
 
+  function findEvenIndex(arr) {
+    for (let i = 0; i <= arr.length; i++) {
+      arr.forEach(num => {
+        if (isNaN(num)) {
+          return -1;
+        }
+      });
+      if (arr.length === 2 && arr[0] === arr[1]) {
+        return -1;
+      }
+      if (arr.length === 1 || arr.length === 0) {
+        return 0;
+      }
+      if (arr.slice(1).reduce((ac, cur) => ac += cur) === 0) {
+        return 0;
+      }
+      if (arr.slice(0, arr.length - 1).reduce((ac, cur) => ac += cur) === 0) {
+        return arr.length - 1;
+      }
+      if (i < arr.length - 2) {
+        if (arr.slice(0, i + 1).reduce((ac, cur) => ac += cur) === arr.slice(i + 2, arr.length).reduce((ac, cur) => ac += cur)) {
+          return i + 1;
+        }
+      } else {
+        if (arr.slice(0, i - 2).reduce((ac, cur) => ac += cur) === arr.slice(i - 1, arr.length).reduce((ac, cur) => ac += cur)) {
+          return i + 1;
+        }
+      }
+      if (i === arr.length) {
+        return -1;
+      }
+    }
+  }
 
+  console.log(findEvenIndex(['l', 'h']));
 });
