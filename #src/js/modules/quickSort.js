44af0c5946dc36b7a7431e34dabfcfbb408d2437
Nazariy <nazarroot3113@gmail.com>
2021-01-30 17:15:06 +0200

    quickSort

---
+++ #src/js/modules/quickSort.js
@@ -0,0 +1,19 @@
+const numArr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+function quickSort(arr) {
+    if (arr.length < 2) return arr;
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (pivot > arr[i]) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right));
+}
+
+console.log(quickSort(numArr));
